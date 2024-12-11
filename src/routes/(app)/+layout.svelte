89bed9cfd4adcef5b26faf22dc0cffcff9952d6b
qty0 <peterszabototh@gmail.com>
2024-12-11 20:59:34 +0100

    x

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -4,12 +4,36 @@
   import { goto } from '$app/navigation';
   import LogoutSvg from '$lib/SVG/LogoutSvg.svelte';
   import { browser } from '$app/environment';
+  import { tweened } from 'svelte/motion';
+  import { quintOut } from 'svelte/easing';
+  let { children } = $props();
 
   let isSidebarExpanded = $state(browser ? localStorage.getItem('isSidebarExpanded') === 'true' : false);
+  let isSidebarHovered: boolean = $state(false);
 
   $effect(() => {
     // cache the sidebar state
     if (browser) localStorage.setItem('isSidebarExpanded', String(isSidebarExpanded));
+  });
+
+  let shouldShowExtendedContent = $state(false);
+
+  $effect(() => {
+    if (isSidebarHovered || isSidebarExpanded) {
+      $sidebarWidth = 20;
+      // set shouldShowExtendedContent to true, but delay it by 100 ms
+      setTimeout(() => {
+        shouldShowExtendedContent = true;
+      }, 25);
+    } else {
+      $sidebarWidth = 5;
+      shouldShowExtendedContent = false;
+    }
+  });
+
+  const sidebarWidth = tweened(20, {
+    duration: 200,
+    easing: quintOut,
   });
 
   //   check if user is logged in
@@ -17,7 +41,7 @@
     const user = await Users.getCurrentUser();
     if (!user) {
       console.log('User not logged in');
-      
+
       // redirect to login page if user is not logged in
       alert('Please login to continue');
       goto('/login');
@@ -28,17 +52,28 @@
 <div class="drawer drawer-open">
   <input id="my-drawer" type="checkbox" class="drawer-toggle" />
   <div class="drawer-content">
-    <slot></slot>
+    {@render children()}
   </div>
   <div class="drawer-side">
     <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-    <aside class={`menu p-4 min-h-full bg-base-200 text-base-content ${isSidebarExpanded ? 'w-80' : ''}`}>
+    <aside
+      class={`menu p-4 min-h-full bg-base-200 text-base-content`}
+      style="width: {$sidebarWidth}rem"
+      onmouseenter={() => {
+        // console.log('hovered');
+        isSidebarHovered = true;
+      }}
+      onmouseleave={() => {
+        // console.log('un-hovered');
+        isSidebarHovered = false;
+      }}
+    >
       <div class="flex flex-row justify-between items-center">
         <span class="font-semibold">
-          {isSidebarExpanded ? 'My App' : ''}
+          {shouldShowExtendedContent ? 'My App' : ''}
         </span>
         <button class="btn btn-ghost btn-square" onclick={() => (isSidebarExpanded = !isSidebarExpanded)}>
-          {#if isSidebarExpanded}
+          {#if shouldShowExtendedContent}
             ←
           {:else}
             🟰
@@ -49,7 +84,7 @@
         <li>
           <a href="/">
             😀
-            {#if isSidebarExpanded}
+            {#if shouldShowExtendedContent}
               Sidebar Item 1
             {/if}
           </a>
@@ -61,7 +96,7 @@
           <span class="scale-x-[-1]">
             <LogoutSvg />
           </span>
-          {#if isSidebarExpanded}
+          {#if shouldShowExtendedContent}
             Log Out
           {/if}
         </button>
